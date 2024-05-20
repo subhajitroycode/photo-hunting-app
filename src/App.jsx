0a28a7d4119125a00f2488b4f7432cab056bd9a9
Subhajit Roy <roysubhajit8697@gmail.com>
2024-05-20 20:27:04 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <main className="main-container">
+      <Header />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="text-center">
+      <h1 className="text-9xl font-jaro">Photo Hunt App</h1>
+      <p className="font-roboto text-2xl mt-2">
+        Let the <span className="italic text-[#ff6600]">hunt</span> begin
+      </p>
+    </header>
+  );
+};
+
+export default Header;
