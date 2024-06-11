bd8f8148f440e2d0395dc078f8cffcfbbe6045b2
Subhajit Roy <roysubhajit8697@gmail.com>
2024-06-11 21:56:07 +0530

    added image hover and transition effect

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import Header from "./components/Header";
 import SearchBox from "./components/SearchBox";
 import DisplayImages from "./components/DisplayImages";
 import fetchImages from "./utils/fetchImage";
+import { TailSpin } from "react-loader-spinner";
 
 function App() {
   const [images, setImages] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!searchTerm) return;
     setPage(1);
     setImages([]);
+    setLoading(true);
     fetchImages(searchTerm)
-      .then((data) => setImages(data.results))
+      .then((data) => {
+        setImages(data.results);
+        setLoading(false);
+      })
       .catch((err) => console.log(err));
   }, [searchTerm]);
 
@@ -30,6 +36,20 @@ function App() {
       <Header />
       <main className="p-4">
         <SearchBox onSearch={(searchItem) => setSearchTerm(searchItem)} />
+        {loading && (
+          <div className="flex justify-center items-center">
+            <TailSpin
+              visible={true}
+              height="80"
+              width="80"
+              color="#EA580C"
+              ariaLabel="tail-spin-loading"
+              radius="1"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        )}
         {images.length > 0 && (
           <>
             <DisplayImages images={images} />
--- src/components/DisplayImages.jsx
+++ src/components/DisplayImages.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { MdOutlineFileDownload } from "react-icons/md";
 
 const DisplayImages = ({ images }) => {
   return (
     <>
       <div className="columns-1 md:columns-2 lg:columns-3 gap-4 m-6">
         {images.map((image) => (
-          <div className="w-full mb-4" key={image.id}>
-            <img src={image.urls.regular} alt="" />
+          <div className="w-full mb-4 image-container" key={image.id}>
+            <img src={image.urls.regular} alt="" className="image" />
+            {/* <a href="" className="download-button">
+              ⬇️
+            </a> */}
+            <div class="overlay">
+              <div class="title">Image Title</div>
+              <a href="#" class="download-btn">
+                <MdOutlineFileDownload size={24} />
+              </a>
+            </div>
           </div>
         ))}
       </div>
